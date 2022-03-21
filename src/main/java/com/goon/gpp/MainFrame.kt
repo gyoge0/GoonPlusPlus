e5855c53a916249fb549357e2355894bd0a4b164
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-03-21 11:59:21 -0400

    Several chunks of code are extracted into methods
    Title bar now displays version
    
    Format
    
    Supress "join declaration with assignment" for the future

--- src/main/java/com/goon/gpp/MainFrame.kt
+++ src/main/java/com/goon/gpp/MainFrame.kt
@@ -8,8 +8,11 @@ import java.io.PrintWriter
 import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
 import javax.swing.*
+import javax.swing.event.ChangeEvent
+import javax.swing.event.ChangeListener
 
 
+@Suppress("JoinDeclarationAndAssignment")
 class MainFrame(startingDir: String? = null) : JFrame() {
 
     /** Label displaying the name of the current file. */
@@ -32,10 +35,10 @@ class MainFrame(startingDir: String? = null) : JFrame() {
      */
     init {
         this.defaultCloseOperation = EXIT_ON_CLOSE
+        this.title = String.format("Goon++ %s", VERSION)
 
-        val pane = this.contentPane
+        // Init a bunch of fields
         this.layout = GridBagLayout()
-        val gbc = GridBagConstraints()
         editorTabBar = JTabbedPane()
         editorTabBar.tabLayoutPolicy = JTabbedPane.SCROLL_TAB_LAYOUT
 
@@ -43,82 +46,16 @@ class MainFrame(startingDir: String? = null) : JFrame() {
         editorTabBar.addTab(startTab.name, startTab.editor)
         editorTabs.add(startTab)
         currentTabIdx = 0
+
+
         setFile(startTab.file)
 
-        GridBagLayout().also { pane.layout = it }
+        nameLabel.alignment = Label.CENTER
 
 
-        var menu: JMenu
-        menu = JMenu("File")
-
-        var menuItem: JMenuItem
-        menuItem = JMenuItem("Open")
-        menuItem.addActionListener {
-            val newTab = EditorTab(editorTabs[currentTabIdx].file.parent)
-            if (!newTab.isUntitled) {
-                editorTabBar.addTab(newTab.name, newTab.editor)
-                setFile(newTab.file)
-                currentTabIdx = editorTabBar.tabCount - 1
-            }
-        }
-        menu.add(menuItem)
-
-        menuItem = JMenuItem("Save")
-        menuItem.addActionListener {
-            if (editorTabs[currentTabIdx].isUntitled) {
-                JOptionPane.showMessageDialog(this, "You must \"Save As\" to set a file location first!", "Error", JOptionPane.ERROR_MESSAGE)
-            } else {
-                val prw = PrintWriter(editorTabs[currentTabIdx].file)
-                for (line in startTab.editor.text.lines()) {
-                    prw.println(line)
-                }
-                prw.close()
-            }
-        }
-        menu.add(menuItem)
-
-        menuItem = JMenuItem("Save As")
-        menuItem.addActionListener {
-            val currentTab = editorTabs[currentTabIdx]
-            currentTab.setFile(currentTab.file.parent)
-            val prw = PrintWriter(currentTab.file)
-            for (line in currentTab.editor.text.lines()) {
-                prw.println(line)
-            }
-            prw.close()
-            setFile(currentTab.file)
-        }
-
-        menu.add(menuItem)
-
-        menuBar.add(menu)
-
-
-        gbc.weightx = 0.0
-        gbc.weighty = 0.0
-        gbc.fill = GridBagConstraints.NONE
-        gbc.gridwidth = 1
-        gbc.gridx = 0
-        gbc.gridy = 0
-        pane.add(menuBar, gbc)
-
-        gbc.weightx = 1.0
-        gbc.weighty = 0.0
-        gbc.fill = GridBagConstraints.HORIZONTAL
-        gbc.gridwidth = 1
-        gbc.gridx = 2
-        gbc.gridy = 0
-        pane.add(nameLabel, gbc)
-
-
-        gbc.weighty = 1.0
-        gbc.fill = GridBagConstraints.BOTH
-        gbc.gridwidth = 5
-        gbc.gridx = 0
-        gbc.gridy = 1
-        pane.add(editorTabBar, gbc)
-
-
+        initMenu()
+        initLayout()
+        initListeners()
 
         this.pack()
         this.isVisible = true
@@ -131,6 +68,118 @@ class MainFrame(startingDir: String? = null) : JFrame() {
         nameLabel.text = String.format("Goon++    |     Editing: %s", file.name)
     }
 
+
+    /**
+     * Listener that sets the file when the tab is changed.
+     */
+    private inner class TabChangedListener : ChangeListener {
+        override fun stateChanged(e: ChangeEvent) {
+            currentTabIdx = editorTabBar.selectedIndex
+            setFile(editorTabs[currentTabIdx].file)
+        }
+    }
+
+    /**
+     * Initializes the menu bar.
+     */
+    private fun initMenu() {
+        val menu: JMenu
+        menu = JMenu("File")
+
+        var menuItem: JMenuItem
+        menuItem = JMenuItem("Open")
+        menuItem.addActionListener {
+            val newTab = EditorTab(editorTabs[currentTabIdx].file.parent)
+            if (!newTab.isUntitled) {
+                editorTabBar.addTab(newTab.name, newTab.editor)
+                editorTabs.add(newTab)
+                currentTabIdx = editorTabBar.tabCount - 1
+            }
+        }
+        menu.add(menuItem)
+
+        menuItem = JMenuItem("Save")
+        menuItem.addActionListener {
+            if (editorTabs[currentTabIdx].isUntitled) {
+                JOptionPane.showMessageDialog(
+                    this,
+                    "You must \"Save As\" to set a file location first!",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+                )
+            } else {
+                val prw = PrintWriter(editorTabs[currentTabIdx].file)
+                for (line in editorTabs[currentTabIdx].textArea.text.lines()) {
+                    prw.println(line)
+                }
+                prw.close()
+            }
+        }
+        menu.add(menuItem)
+
+        menuItem = JMenuItem("Save As")
+        menuItem.addActionListener {
+            val currentTab = editorTabs[currentTabIdx]
+            currentTab.setFile(currentTab.file.parent)
+            val prw = PrintWriter(currentTab.file)
+            for (line in currentTab.textArea.text.lines()) {
+                prw.println(line)
+            }
+            prw.close()
+            setFile(currentTab.file)
+        }
+
+        menu.add(menuItem)
+
+        menuBar.add(menu)
+    }
+
+    /**
+     * Initializes the layout with all the elements.
+     */
+    private fun initLayout() {
+        val pane = this.contentPane
+        var gbc: GridBagConstraints
+
+        gbc = GridBagConstraints()
+        gbc.weightx = 0.0
+        gbc.weighty = 0.0
+        gbc.fill = GridBagConstraints.NONE
+        gbc.gridwidth = 1
+        gbc.gridx = 0
+        gbc.gridy = 0
+        pane.add(menuBar, gbc)
+
+        gbc = GridBagConstraints()
+        gbc.weightx = 1.0
+        gbc.weighty = 0.0
+        gbc.fill = GridBagConstraints.HORIZONTAL
+        gbc.gridwidth = 1
+        gbc.gridx = 2
+        gbc.gridy = 0
+        pane.add(nameLabel, gbc)
+
+
+        gbc = GridBagConstraints()
+        gbc.weighty = 1.0
+        gbc.fill = GridBagConstraints.BOTH
+        gbc.gridwidth = 5
+        gbc.gridx = 0
+        gbc.gridy = 1
+        pane.add(editorTabBar, gbc)
+
+    }
+
+    /**
+     * Adds all the required listeners to components.
+     */
+    private fun initListeners() {
+        editorTabBar.addChangeListener(TabChangedListener())
+    }
+
+    /**
+     * Plays the sound at the path specified.
+     */
     @Synchronized
     fun playSound(path: String) {
         Thread {
@@ -147,4 +196,10 @@ class MainFrame(startingDir: String? = null) : JFrame() {
         }.start()
     }
 
+    companion object {
+        /**
+         * Version number.
+         */
+        const val VERSION = "0.1"
+    }
 }
