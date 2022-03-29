5227e1cf747f7d0620f5e069baf3d0342144b1ae
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-03-29 17:31:43 -0400

    Load config

--- gpp-core/src/main/kotlin/com/gyoge/gpp/Main.kt
+++ gpp-core/src/main/kotlin/com/gyoge/gpp/Main.kt
@@ -1,6 +1,9 @@
 package com.gyoge.gpp
 
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
+import java.io.File
 import javax.swing.UIManager
 
 
@@ -13,7 +16,25 @@ val format = Json {
 fun main(args: Array<String>) {
     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
 
-    val config = Config()
+    val configDir = File("${System.getProperty("user.home")}/.gpp")
+    val configFile = File("${configDir.absolutePath}/config.json")
+    val config: Config
+
+    if (!configDir.exists()) {
+        configDir.mkdir()
+        configFile.createNewFile()
+        config = Config()
+        configFile.writeText(format.encodeToJsonElement(config).toString())
+    } else if (!configFile.exists()) {
+        configFile.createNewFile()
+        config = Config()
+        configFile.writeText(format.encodeToJsonElement(config).toString())
+    } else {
+        config = format.decodeFromString(configFile.readText())
+    }
+
+    println("config = $config")
+
 
     if (args.isEmpty()) {
         val mf = MainFrame(config)
