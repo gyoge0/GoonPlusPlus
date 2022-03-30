66f95acee27168f4fb2f9205b5ec5e36e947b579
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-03-30 19:11:17 -0400

    Imports + set size of preferences window

--- gpp-core/src/main/kotlin/com/gyoge/gpp/PreferencesFrame.kt
+++ gpp-core/src/main/kotlin/com/gyoge/gpp/PreferencesFrame.kt
@@ -1,7 +1,13 @@
 package com.gyoge.gpp
 
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.BoxLayout
 import javax.swing.JFrame
+import javax.swing.JLabel
 import javax.swing.JPanel
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaField
 
 class PreferencesFrame(private val config: Config) : JFrame() {
     init {
@@ -24,7 +30,13 @@ class PreferencesFrame(private val config: Config) : JFrame() {
         this.pack()
         this.isLocationByPlatform = true
         this.isVisible = true
+
+        // Setting size of the window
+        // This would be at the top of the method, but it doesn't work for some reason
+        this.isResizable = true
+        this.size = Dimension(400, 400)
         this.isResizable = false
+
         this.requestFocus()
 
     }
