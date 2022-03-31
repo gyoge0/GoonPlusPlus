7270db077d975f30d08e83091e634208a2dea051
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-03-30 20:22:39 -0400

    Add in basic 2 column preference viewing

--- gpp-core/src/main/kotlin/com/gyoge/gpp/PreferencesFrame.kt
+++ gpp-core/src/main/kotlin/com/gyoge/gpp/PreferencesFrame.kt
@@ -1,19 +1,27 @@
 package com.gyoge.gpp
 
-import java.awt.BorderLayout
+import com.gyoge.gpp.filters.AnyFilter
+import com.gyoge.gpp.filters.BooleanFilter
+import com.gyoge.gpp.filters.DoubleFilter
+import com.gyoge.gpp.filters.IntFilter
 import java.awt.Dimension
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
 import javax.swing.BoxLayout
 import javax.swing.JFrame
-import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.JTextField
+import javax.swing.text.PlainDocument
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.javaField
+
 
 class PreferencesFrame(private val config: Config) : JFrame() {
     init {
         val panel = JPanel()
         this.defaultCloseOperation = DISPOSE_ON_CLOSE
         this.title = String.format("Goon++ :  Preferences   |   v%s", MainFrame.VERSION)
+        this.layout = GridBagLayout()
 
 
         // Init a 2 column design
@@ -21,20 +29,58 @@ class PreferencesFrame(private val config: Config) : JFrame() {
         val values = JPanel()
         names.layout = BoxLayout(names, BoxLayout.Y_AXIS)
         values.layout = BoxLayout(values, BoxLayout.Y_AXIS)
-        this.add(names, BorderLayout.WEST)
-        this.add(values, BorderLayout.EAST)
+
 
 
         config::class.memberProperties.forEach { p ->
             val javaField = p.javaField
             if (javaField != null && javaField.isAnnotationPresent(PrettyName::class.java)) {
-                val name = JLabel()
+                val name = JTextField()
                 name.text = javaField.getAnnotation(PrettyName::class.java).name
+                name.horizontalAlignment = JTextField.CENTER
+                name.isEditable = false
                 names.add(name)
 
-                // values
+                val value = JTextField()
+                value.text = p.getter.call(config).toString()
+
+                val doc = value.document as PlainDocument
+                doc.documentFilter = when (javaField.type) {
+                    String::class.java -> {
+                        AnyFilter()
+                    }
+                    Double::class.java -> {
+                        DoubleFilter()
+                    }
+                    Int::class.java -> {
+                        IntFilter()
+                    }
+                    Boolean::class.java -> {
+                        BooleanFilter()
+                    }
+                    else -> {
+                        AnyFilter()
+                    }
+                }
+
+                value.horizontalAlignment = JTextField.CENTER
+                value.isEditable = true
+                values.add(value)
+
             }
         }
+
+        val gbc = GridBagConstraints()
+        gbc.weightx = 1.0
+        gbc.fill = GridBagConstraints.HORIZONTAL
+        gbc.gridwidth = 1
+        gbc.gridy = 0
+
+        gbc.gridx = 0
+        this.add(names, gbc)
+        gbc.gridx = 1
+        this.add(values, gbc)
+
 
         this.add(panel)
         this.pack()
