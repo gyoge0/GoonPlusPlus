fa8b507e7f7e3faa00401dab65699dcd6b4afe5a
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-04-04 12:17:36 -0400

    Changed how names and types are stored inside the serialized json

--- gpp-core/src/main/kotlin/com/gyoge/gpp/Config.kt
+++ gpp-core/src/main/kotlin/com/gyoge/gpp/Config.kt
@@ -1,14 +1,19 @@
 package com.gyoge.gpp
 
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Config(
-    @PrettyName("Font Name") var fontName: String = "Comic Sans MS",
-    @PrettyName("Font Size") var fontSize: Int = 12,
-    @PrettyName("Font Color") var fontColor: Int = 0x000000,
-    @PrettyName("Background Color") var backgroundColor: Int = 0xFFFFFF,
+    @SerialName("Font Name") var fontName: ConfigPair<String> = ConfigPair("Comic Sans MS"),
+    @SerialName("Font Size") var fontSize: ConfigPair<Int> = ConfigPair(12),
+    @SerialName("Font Color") var fontColor: ConfigPair<Int> = ConfigPair(0x000000),
+    @SerialName("Background Color") var backgroundColor: ConfigPair<Int> = ConfigPair(0xFFFFFF),
 )
 
-@Target(AnnotationTarget.FIELD)
-annotation class PrettyName(val name: String)
+@Serializable
+data class ConfigPair<T : Any>(
+    @SerialName("v") @Contextual val value: T,
+    @SerialName("t") val type: String = value::class.simpleName!!
+)
