74a1f78e9a35c5d43fbe8a80baf4a1c5dbd0e1d1
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-07-22 17:37:27 -0400

    Textblocks with icons for different explorer nodes

---
+++ GoonPlusPlus/Controls/ExplorerTree/EmptyFolderItem.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvaloniaEdit;
+
+namespace GoonPlusPlus.Controls.ExplorerTree;
+
+public partial class EmptyFolderItem : UserControl
+{
+    public TextBlock TextBlock { get; }
+
+    public EmptyFolderItem()
+    {
+        InitializeComponent();
+        TextBlock = this.FindControl<TextBlock>("Block");
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
