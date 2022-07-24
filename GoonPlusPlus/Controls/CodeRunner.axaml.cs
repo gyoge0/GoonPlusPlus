db8660b9d97647721b550a6d3d6f70022b2c8067
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-07-24 10:07:37 -0400

    Create code runner control

---
+++ GoonPlusPlus/Controls/CodeRunner.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GoonPlusPlus.Controls;
+
+public partial class CodeRunner : UserControl
+{
+    public CodeRunner()
+    {
+        InitializeComponent();
+        this.FindControl<TabItem>("Compile");
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
