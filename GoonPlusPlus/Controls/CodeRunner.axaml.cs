2b0c45aa120d4a8dcae15aa7e1105f5d8aa2a764
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-07-26 18:12:42 -0400

    Allow collapsing of compile tab

--- GoonPlusPlus/Controls/CodeRunner.axaml.cs
+++ GoonPlusPlus/Controls/CodeRunner.axaml.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 
 namespace GoonPlusPlus.Controls;
 
@@ -15,5 +17,17 @@ public partial class CodeRunner : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void Collapse_OnTapped(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not TabItem item) return;
+        var open = !item.IsSelected;
+        item.IsSelected = open;
+        item.Parent!.FindDescendantOfType<TextBox>().IsVisible = open;
+        var grid = item.FindAncestorOfType<Grid>();
+        grid.RowDefinitions[2].Height = new GridLength(open ? 250 : 60);
+        grid.RowDefinitions[2].MinHeight = open ? 120 : 60;
+        (grid.Children[1] as GridSplitter)!.IsEnabled = open;
     }
 }
