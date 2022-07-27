9eac5d5a2fa5b2ec19dc8e3b7391fe9cdd72ad7c
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-07-27 08:41:24 -0400

    Add tab for run

--- GoonPlusPlus/Controls/CodeRunner.axaml.cs
+++ GoonPlusPlus/Controls/CodeRunner.axaml.cs
@@ -21,13 +21,23 @@ public partial class CodeRunner : UserControl
 
     private void Collapse_OnTapped(object? sender, RoutedEventArgs e)
     {
-        if (sender is not TabItem item) return;
-        var open = !item.IsSelected;
-        item.IsSelected = open;
-        item.Parent!.FindDescendantOfType<TextBox>().IsVisible = open;
+        if (sender is not TabItem { Content: TextBox box } item) return;
+        var open = !box.IsVisible;
+        box.IsVisible = open;
         var grid = item.FindAncestorOfType<Grid>();
         grid.RowDefinitions[2].Height = new GridLength(open ? 250 : 60);
         grid.RowDefinitions[2].MinHeight = open ? 120 : 60;
         (grid.Children[1] as GridSplitter)!.IsEnabled = open;
+    }
+
+    private void TabControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs _)
+    {
+        if (sender is not TabControl { SelectedContent: TextBox box } control) return;
+        box.IsVisible = true;
+        var grid = control.FindAncestorOfType<Grid>();
+        if (grid == null) return;
+        grid.RowDefinitions[2].Height = new GridLength(250);
+        grid.RowDefinitions[2].MinHeight = 120;
+        (grid.Children[1] as GridSplitter)!.IsEnabled = true;
     }
 }
