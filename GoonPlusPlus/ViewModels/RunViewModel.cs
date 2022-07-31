7a9a38d22b1ca79defb3bf15f416eaee22a20dec
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-07-31 17:09:33 -0400

    Refactor runviewmodel to work with process

--- GoonPlusPlus/ViewModels/RunViewModel.cs
+++ GoonPlusPlus/ViewModels/RunViewModel.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
+using System.IO;
+using System.Reactive.Linq;
 using System.Text;
 using DynamicData;
-using GoonPlusPlus.Models;
 using ReactiveUI;
 
 namespace GoonPlusPlus.ViewModels;
@@ -12,28 +11,118 @@ namespace GoonPlusPlus.ViewModels;
 public class RunViewModel : ViewModelBase
 {
     public static RunViewModel Instance { get; private set; } = null!;
+
+    public static readonly string Appdata =
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Goon++/";
+
+    private Process? _runProcess;
+    private StringBuilder _stdInBuilder = new();
+    private StringBuilder _editingBuilder = new();
+    private int _vCaretIndex;
+    private int _accessCaretIndex;
+    private bool _caretCanMove;
+    private string _text = string.Empty;
+
+    #region ReactiveProperties
+
+    public Process? RunProcess
+    {
+        get => _runProcess;
+        set => this.RaiseAndSetIfChanged(ref _runProcess, value);
+    }
+
+    public StringBuilder StdInBuilder
+    {
+        get => _stdInBuilder;
+        set => this.RaiseAndSetIfChanged(ref _stdInBuilder, value);
+    }
+
+    public StringBuilder EditingBuilder
+    {
+        get => _editingBuilder;
+        set => this.RaiseAndSetIfChanged(ref _editingBuilder, value);
+    }
+
+    public int VCaretIndex
+    {
+        get => _vCaretIndex;
+        set => this.RaiseAndSetIfChanged(ref _vCaretIndex, value);
+    }
+
+    public int AccessCaretIndex
+    {
+        get => _accessCaretIndex;
+        set => this.RaiseAndSetIfChanged(ref _accessCaretIndex, value);
+    }
+
+    public bool CaretCanMove
+    {
+        get => _caretCanMove;
+        set => this.RaiseAndSetIfChanged(ref _caretCanMove, value);
+    }
+
+    public string Text
+    {
+        get => _text;
+        set => this.RaiseAndSetIfChanged(ref _text, value);
+    }
+
+    #endregion
+
+
+    #region StandardBuffers
+
     public SourceList<string> StdOut { get; } = new();
     public SourceList<string> StdErr { get; } = new();
-    public Process? RunProcess { get; set; }
-    private readonly Queue<string> _stdIn = new();
-    public StringBuilder StdInBuilder { get; } = new();
+    public SourceList<string> StdIn { get; } = new();
 
-    public void Enqueue()
-    {
-        if (RunProcess == null) _stdIn.Enqueue(StdInBuilder.ToString());
-        else RunProcess.StandardInput.WriteLine(StdInBuilder.ToString());
-        StdInBuilder.Clear();
-    }
+    #endregion
 
     public RunViewModel()
     {
         Instance = this;
+
         this.WhenAnyValue(x => x.RunProcess)
             .WhereNotNull()
-            .Subscribe(p =>
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(_ =>
             {
-                _stdIn.ToList().ForEach(l => p.StandardInput.WriteLineAsync(l));
-                _stdIn.Clear();
+                Text = string.Empty;
+                VCaretIndex = 0;
+                AccessCaretIndex = 0;
+                StdIn.Clear();
             });
+
+        StdOut
+            .Connect()
+            .OnItemAdded(l =>
+            {
+                // The process adds a null character when it terminates
+                // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+                if (l == null) return;
+
+                StdInBuilder.Append(EditingBuilder);
+                EditingBuilder.Clear();
+                VCaretIndex = 0;
+                // ReSharper disable once NullCoalescingConditionIsAlwaysNotNullAccordingToAPIContract
+                Text ??= string.Empty;
+
+                Text = Text.Insert(AccessCaretIndex, l);
+                AccessCaretIndex += l.Length;
+            })
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe();
+
+        StdIn
+            .Connect()
+            .OnItemAdded(l =>
+            {
+                // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+                if (l == null) return;
+                if (RunProcess == null) return;
+                RunProcess.StandardInput.WriteLine(l);
+            })
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe();
     }
 }
