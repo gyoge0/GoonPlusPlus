d4a3afda5fd36068f470832e2c2da6311a6a63d1
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-07-31 17:08:53 -0400

    Include viewmodel for swapping bottom bar tabs

---
+++ GoonPlusPlus/ViewModels/BottomBarTabViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+
+namespace GoonPlusPlus.ViewModels;
+
+public class BottomBarTabViewModel : ViewModelBase
+{
+    public static BottomBarTabViewModel Instance { get; private set; }
+    private int _currentTabIdx;
+
+    public int CurrentTabIdx
+    {
+        get => _currentTabIdx;
+        set => this.RaiseAndSetIfChanged(ref _currentTabIdx, value);
+    }
+
+    public BottomBarTabViewModel()
+    {
+        Instance = this;
+    }
+
+    public enum TabIdx
+    {
+        Compile,
+        Run,
+    }
+}
