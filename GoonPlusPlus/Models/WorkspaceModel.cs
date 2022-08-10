d37562965cc3ad2c757f1595ca11f40681aa1426
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-08-10 08:47:25 -0400

    Create class modeling a workspace

---
+++ GoonPlusPlus/Models/WorkspaceModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using GoonPlusPlus.ViewModels;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace GoonPlusPlus.Models;
+
+[JsonObject(MemberSerialization.OptOut, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+public class WorkspaceModel
+{
+    public List<string> Classpath { get; } = new();
+    public string RootPath { get; set; }
+    public string OutputDir { get; set; }
+    public List<TabModel> Tabs { get; } = new();
+
+    public WorkspaceModel(string rootPath)
+    {
+        RootPath = rootPath;
+        OutputDir = rootPath;
+    }
+
+    public static WorkspaceModel? Load(string path) =>
+        JsonConvert.DeserializeObject<WorkspaceModel>(File.ReadAllText(path));
+
+    public void Save(string path) => File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+}
