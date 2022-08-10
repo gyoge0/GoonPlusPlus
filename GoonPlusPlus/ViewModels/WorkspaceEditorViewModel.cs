f6f4e0981fdd0e0949fce1f192c6cd8d7fb22240
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-08-10 14:26:10 -0400

    Create dialog for editing the current workspace

---
+++ GoonPlusPlus/ViewModels/WorkspaceEditorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using DynamicData;
+using ReactiveUI;
+
+namespace GoonPlusPlus.ViewModels;
+
+public class WorkspaceEditorViewModel : ViewModelBase
+{
+    private readonly ReadOnlyObservableCollection<string> _cpItems;
+
+    public ReadOnlyObservableCollection<string> CpItems => _cpItems;
+    public string? Selection { get; set; }
+
+    public WorkspaceEditorViewModel()
+    {
+        var wksp = WorkspaceViewModel.Instance.Workspace!;
+        _cpItems = new ReadOnlyObservableCollection<string>(new ObservableCollection<string>(wksp.Classpath.Items));
+        wksp.Classpath
+            .Connect()
+            .Bind(out _cpItems)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe();
+
+        RemoveItems = ReactiveCommand.Create(() => { wksp.Classpath!.Remove(Selection); });
+
+        AddItems = ReactiveCommand.CreateFromTask(async (Window source) =>
+            (await new OpenFileDialog { AllowMultiple = true }.ShowAsync(source))?
+            .Where(i => !wksp.Classpath.Items.Contains(i))
+            .ToList()
+            .ForEach(p => wksp.Classpath.Add(p)));
+    }
+
+    public ReactiveCommand<Unit, Unit> RemoveItems { get; }
+    public ReactiveCommand<Window, Unit> AddItems { get; }
+}
---
+++ GoonPlusPlus/Views/WorkspaceEditor.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GoonPlusPlus.Views;
+
+public partial class WorkspaceEditor : Window
+{
+    public WorkspaceEditor()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
