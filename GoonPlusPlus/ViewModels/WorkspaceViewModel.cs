a444d94c0d760221cf38978d425142d6887f2f11
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-08-10 08:47:45 -0400

    Create viewmodel for managing current workspace

---
+++ GoonPlusPlus/ViewModels/WorkspaceViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GoonPlusPlus.Models;
+using ReactiveUI;
+
+namespace GoonPlusPlus.ViewModels;
+
+public class WorkspaceViewModel : ViewModelBase
+{
+    public static WorkspaceViewModel Instance { get; private set; } = null!;
+
+    private WorkspaceModel? _workspace;
+
+    public WorkspaceModel? Workspace
+    {
+        get => _workspace;
+        set => this.RaiseAndSetIfChanged(ref _workspace, value);
+    }
+
+
+    public delegate void InstantiatedEventHandler(object sender);
+
+    public static event InstantiatedEventHandler? Instantiated;
+    private void OnInstantiated() => Instantiated?.Invoke(this);
+
+    public WorkspaceViewModel()
+    {
+        Instance = this;
+        OnInstantiated();
+    }
+}
