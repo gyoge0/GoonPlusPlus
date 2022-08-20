51ffaf60358aa619f1c75fc927dcf0f5e92aefc6
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-08-20 14:07:52 -0400

    Save workspace on exit

--- GoonPlusPlus/App.axaml.cs
+++ GoonPlusPlus/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using GoonPlusPlus.Workspace;
 
 namespace GoonPlusPlus;
 
@@ -11,7 +12,10 @@ public class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
             desktop.MainWindow = new MainWindow { DataContext = new MainWindowViewModel(desktop.Args) };
+            desktop.Exit += (_, _) => WorkspaceViewModel.Instance.Workspace?.Save();
+        }
 
         base.OnFrameworkInitializationCompleted();
     }
