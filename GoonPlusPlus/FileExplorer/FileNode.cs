fb40e7d7f4c8b2f803527069822a8d435d016553
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-08-30 19:02:57 -0400

    Add python as a runnable file extension

--- GoonPlusPlus/FileExplorer/FileNode.cs
+++ GoonPlusPlus/FileExplorer/FileNode.cs
@@ -2,9 +2,16 @@
 
 public class FileNode : ExplorerNode
 {
-    public static readonly string[] CompilableExtensions = { "java" };
+    public static readonly string[] CompilableExtensions =
+    {
+        "java",
+    };
 
-    public static readonly string[] RunnableExtensions = { "java" };
+    public static readonly string[] RunnableExtensions =
+    {
+        "java",
+        "py"
+    };
 
     public FileNode(string strFullPath) : base(strFullPath) { }
 
